619c4f4465c400515a8adeee309f1ac6120a78a7
sazzad <sazzadador@gmail.com>
2024-12-29 13:23:01 +0600

    change repo link

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // midleware
 app.use(cors());
 app.use(express.json());
-
+// DB_USER=carRental
+// DB_PASS=Ador123
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krhx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,10 +27,7 @@ async function run() {
   const carsCollection = client.db("car-rental").collection("cars");
   const bookCollection = client.db("car-rental").collection("booked");
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
